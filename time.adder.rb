73df85e304df38798fe6bc9a3d1582cf54e33f4f
rikifujihara <rikifujihara@gmail.com>
2022-03-29 21:31:42 +1100

    can now return total hours, minutes and seconds from total secs and mins

--- time.adder.rb
+++ time.adder.rb
@@ -9,62 +9,49 @@ eg. 1.12
 
 Hit enter on an empty line when you've entered all of the times you'd like to add up."
 
-
-
-# time_durations = Array.new
-
-# while time_durations.last != ""
-#         time_durations << gets.chomp
-# end
-
-# time_durations.delete_at(-1)
-
-# use % to have remainders
-
-#This first time, I'm going to try splitting the gets.chomp into a minutes array and a seconds array
-
+input_count = 0
 minutes_array = Array.new
 seconds_array = Array.new
-chomped_string = "empty"
+chomped_string = "empty.:"
 
 while chomped_string != ""
-        # if chomped_string.include? "."
         chomped_string = gets.chomp
+        if chomped_string.include? "."
         user_input = chomped_string.split(".")
         minutes_array << user_input[0].to_i
         seconds_array << user_input[1].to_i
-        # elsif chomped_string.include? ":"
-        # chomped_string = gets.chomp
-        # user_input = chomped_string.split(":")
-        # total_minutes << user_input[0]
-        # total_seconds << user_input[1]
-        # else
-        # puts "I don't know how to deal with your input"
-        # chomped_string = ""
-        # end
+        input_count += 1
+        puts "You've entered #{input_count} duration(s)"
+        elsif chomped_string.include? ":"
+            user_input = chomped_string.split(":")
+            minutes_array << user_input[0].to_i
+            seconds_array << user_input[1].to_i
+            input_count += 1
+            puts "You've entered #{input_count} duration(s)"
+        end
 end
 
-# puts "------------"
-# puts minutes_array
-# puts "------------"
-# puts seconds_array
-
-
 def sum_of(array)
-
     total = 0
-
     array.each do |item|
         total += item
     end
-
     return total
-
 end
 
 total_minutes = sum_of(minutes_array)
-# puts total_minutes
-# puts "---------"
 total_seconds = sum_of(seconds_array)
-# puts total_seconds
 
+hours = 0
+minutes = 0
+seconds = 0
+
+#deal with the total seconds
+seconds += total_seconds % 60
+minutes += total_seconds / 60
+
+#deal with the total minutes
+minutes += total_minutes % 60
+hours += total_minutes / 60
+
+puts "The total duration of the inputs is #{hours} hours, #{minutes} minutes and #{seconds} seconds."
