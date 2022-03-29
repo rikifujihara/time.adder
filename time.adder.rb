9b361c3299d48b8134b6fbe92f8eb17ec7bf4599
rikifujihara <rikifujihara@gmail.com>
2022-03-29 20:53:40 +1100

    created two variables - total minutes and total seconds

--- time.adder.rb
+++ time.adder.rb
@@ -23,16 +23,16 @@ Hit enter on an empty line when you've entered all of the times you'd like to ad
 
 #This first time, I'm going to try splitting the gets.chomp into a minutes array and a seconds array
 
-total_minutes = Array.new
-total_seconds = Array.new
-chomped_string = "empty:"
+minutes_array = Array.new
+seconds_array = Array.new
+chomped_string = "empty"
 
 while chomped_string != ""
         # if chomped_string.include? "."
         chomped_string = gets.chomp
         user_input = chomped_string.split(".")
-        total_minutes << user_input[0]
-        total_seconds << user_input[1]
+        minutes_array << user_input[0].to_i
+        seconds_array << user_input[1].to_i
         # elsif chomped_string.include? ":"
         # chomped_string = gets.chomp
         # user_input = chomped_string.split(":")
@@ -43,3 +43,28 @@ while chomped_string != ""
         # chomped_string = ""
         # end
 end
+
+# puts "------------"
+# puts minutes_array
+# puts "------------"
+# puts seconds_array
+
+
+def sum_of(array)
+
+    total = 0
+
+    array.each do |item|
+        total += item
+    end
+
+    return total
+
+end
+
+total_minutes = sum_of(minutes_array)
+# puts total_minutes
+# puts "---------"
+total_seconds = sum_of(seconds_array)
+# puts total_seconds
+
